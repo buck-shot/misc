5c364c1ec9620e857dad5cfcd6debc0a10aa9a8c
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-16 19:44:41 -0400

    fixed

---
+++ notification.lua
@@ -0,0 +1,7 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+function loaded()
+  Notification.new("success", "Success!", "Successfully loaded " .. sName .. "!", true, 5)
+end
+function loading()
+  Notification.new("info", "Loading", "Please wait until the game loads...", true, 10)
+end
