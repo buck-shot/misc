374a50658ed776051128b0079ae1a5f377fcfff4
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-17 13:51:04 -0400

    disable the vehicles folder ac

--- disable-vehicle-ac.lua
+++ disable-vehicle-ac.lua
@@ -10,21 +10,8 @@ end)
 
 for i = 1, 20 do
     task.wait(0.5)
-    for _, connection in pairs(getconnections(game:GetService("ReplicatedStorage").MatchTimer.Changed)) do
-        connection:Disable()
-    end
-    for i, connection in pairs(getconnections(game:GetService("Workspace").Spawns.ChildAdded)) do
-        connection:Disable()           
-    end
-
     for i, connection in pairs(getconnections(game:GetService("Workspace").Vehicles.DescendantAdded)) do
         connection:Disable()           
-    end
-
-    for _, descendants in pairs(game:GetService("Workspace").Spawns:GetDescendants()) do
-        for _, connections in pairs(getconnections(descendants.Changed)) do
-            connections:Disable()
-        end
     end
 
     for _, descendants in pairs(game:GetService("Workspace").Vehicles:GetDescendants()) do
