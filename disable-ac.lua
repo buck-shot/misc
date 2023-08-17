d3e47c154cf2e8df27523422d904ad52c53a7908
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-17 12:43:33 -0400

    Create disable-ac.lua

---
+++ disable-ac.lua
@@ -0,0 +1,35 @@
+local xx = game.Players.LocalPlayer
+local oldhmmnc  
+oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
+    if self == xx and getnamecallmethod():lower() == "kick" or not checkcaller() and getnamecallmethod() == "Destroy" and self.Name == "Highlight" then
+        return
+    end
+    
+    return oldhmmnc(self, ...)
+end)
+
+for i = 1, 20 do
+    task.wait(0.5)
+    for _, connection in pairs(getconnections(game:GetService("ReplicatedStorage").MatchTimer.Changed)) do
+        connection:Disable()
+    end
+    for i, connection in pairs(getconnections(game:GetService("Workspace").Spawns.ChildAdded)) do
+        connection:Disable()           
+    end
+
+    for i, connection in pairs(getconnections(game:GetService("Workspace").Vehicles.DescendantAdded)) do
+        connection:Disable()           
+    end
+
+    for _, descendants in pairs(game:GetService("Workspace").Spawns:GetDescendants()) do
+        for _, connections in pairs(getconnections(descendants.Changed)) do
+            connections:Disable()
+        end
+    end
+
+    for _, descendants in pairs(game:GetService("Workspace").Vehicles:GetDescendants()) do
+        for _, connections in pairs(getconnections(descendants.Changed)) do
+            connections:Disable()
+        end
+    end
+end
