6093b3feed4261caadde271be62d25a262486324
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-16 21:03:53 -0400

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,3 +1,7 @@
+while game:GetService("Workspace").DistributedGameTime <= 12 do
+    wait(1)
+end
+print("initial")
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 function loaded()
   Notification.new("success", "Success!", "Successfully loaded " .. sName .. "!", true, 5)
