6936a09a091918df50d816030a9fb1bd99b06bdb
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-19 22:33:12 -0400

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,11 +1,23 @@
 print("px/executed")
+
 while game:GetService("Workspace").DistributedGameTime <= 15 do
     wait(1)
 end
+
 print("initial")
+
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+
+if game:GetService("Players").LocalPlayer.Name == "AlmostDivine" then
+    Notification.new("warning", "Unauthorized", "You're unauthorized to use this script.", true, 5)
+    print("unauthorized")
+    return
+end
+
+print("authorized")
+
 function loaded()
-  Notification.new("success", "Success!", "Successfully loaded " .. sName .. "!", true, 5)
+  Notification.new("success", "Authorized", "Successfully loaded " .. sName .. "!", true, 5)
 end
 
 Notification.new("info", "Loading", "Please wait until the script finishes loading...", true, 3)
