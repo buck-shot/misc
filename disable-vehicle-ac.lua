d5429790baba71cf8349cdc3f24b9e852478fde9
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-19 22:08:20 -0400

    Update disable-vehicle-ac.lua

--- disable-vehicle-ac.lua
+++ disable-vehicle-ac.lua
@@ -8,20 +8,6 @@ oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
     return oldhmmnc(self, ...)
 end)
 
-local count = 0
+game:GetService("ReplicatedStorage"):WaitForChild("ReplicatorChassis"):Destroy()
 
-for i = 1, 20 do
-    task.wait(0.5)
-    count = count + 1
-    print(count)
-    for i, connection in pairs(getconnections(game:GetService("Workspace").Vehicles.DescendantAdded)) do
-        connection:Disable()           
-    end
-
-    for _, descendants in pairs(game:GetService("Workspace").Vehicles:GetDescendants()) do
-        for _, connections in pairs(getconnections(descendants.Changed)) do
-            connections:Disable()
-        end
-    end
-end
 print("px/bypass")
