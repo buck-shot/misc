8403843a84d3952557d07538eeddf8159c0966a6
buck-shot <93498779+buck-shot@users.noreply.github.com>
2023-08-20 14:29:52 -0400

    Update disable-ac.lua

--- disable-ac.lua
+++ disable-ac.lua
@@ -8,21 +8,6 @@ oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
     return oldhmmnc(self, ...)
 end)
 
-local mt = getrawmetatable(game)
-make_writeable(mt)
-
-local namecall = mt.__namecall
-
-mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    local args = {...}
-
-    if method == "FireServer" and args == {true, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil} then
-        return wait(9e9)
-    end
-    return namecall(self, ...)
-end)
-
 pcall(function()
     game:GetService("StarterPlayer").StarterPlayerScripts:WaitForChild("bo"):Destroy()
     game:GetService("Players").LocalPlayer.PlayerScripts:WaitForChild("bo"):Destroy()
